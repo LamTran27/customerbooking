441617808de1687acf5d404da2e06be41a2f503b
LamTran27 <lam.liam.tran@gmail.com>
2025-09-09 15:36:57 +0700

    update 5

--- customerbooking/settings.py
+++ customerbooking/settings.py
@@ -57,7 +57,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # thêm dòng này
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'customerbooking.urls'
 
@@ -166,11 +169,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # nơi Django gom static khi deploy
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]  # nơi bạn để ảnh khi dev
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
